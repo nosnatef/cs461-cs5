c9d1bb2bbb92d9d302c150c068e8709c1dc2bc06
evanrnewman <newmanrevan@gmail.com>
2020-02-11 18:30:00 -0800

    fixed loading the model and outputting correctly

--- load_model.py
+++ load_model.py
@@ -1,14 +1,18 @@
 import torch
+import numpy as np
 from model.simple import LinearRegression
 
 
 def main():
     model = LinearRegression()
-    model.load_state_dict(torch.load('simple.pt'))
+    model.load_state_dict(torch.load('model/saved_models/simple.pt'))
     model.eval()
 
-    prediction = model(12)
-    print('prediction: ', prediction)
+    a = np.array([12])
+    a = torch.Tensor(a)
+    prediction = model(a)
+    prediction = prediction.detach().numpy()
+    print('prediction: ', prediction[0])
 
 
 if __name__ == "__main__":
