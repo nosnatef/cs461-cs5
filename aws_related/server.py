44a936b9a5cd71925eb02e593cdaf33d30abffa1
Ubuntu <ubuntu@ip-172-31-3-174.us-east-2.compute.internal>
2020-02-14 02:44:44 +0000

    improve connection

--- aws_related/server.py
+++ aws_related/server.py
@@ -1,4 +1,5 @@
 import socket
+import time
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind(('0.0.0.0', 1234))
@@ -9,4 +10,11 @@ while True:
     clientsocket, address = s.accept()
     print("Connection from has been established.")
     clientsocket.send(bytes("Hey there!!!"))
+    while True:
+        data = clientsocket.recv(1024)
+        time.sleep(1)
+        if not data or data.decode('utf-8') == 'exit':
+            break
+        print("Data: %s, Size: %s" % (data.decode('utf-8'), len(data)))
+        clientsocket.send('Data Receive')
     clientsocket.close()
