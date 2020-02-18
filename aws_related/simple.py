517636f3628ceb5321eaecaa07127399eddb5ed8
evanrnewman <newmanrevan@gmail.com>
2020-02-17 19:40:03 -0800

    added machine learning code necessary for AWS

---
+++ aws_related/simple.py
@@ -0,0 +1,13 @@
+import numpy as np
+import pickle
+import torch
+
+class LinearRegression(torch.nn.Module):
+    def __init__(self):
+        super(LinearRegression, self).__init__()
+        self.linear = torch.nn.Linear(1, 1)    
+
+    def forward(self, x):
+        y_pred = self.linear(x)
+        return y_pred
+
